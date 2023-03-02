c9250d0b7023fc259bff172a13b3954003b7144e
Badboynu17 <badboynu17@gmail.com>
2023-03-02 23:43:01 +0300

    added login (beta)

---
+++ src/components/Profile/ProfileContainer.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Profile from './Profile'
+import axios from "axios";
+import {connect} from "react-redux";
+import {setUserProfile} from "../../redux/profile-reduser";
+import {useLocation, useNavigate, useParams,
+} from "react-router-dom";
+
+
+
+class ProfileContainer extends React.Component {
+
+    componentDidMount() {
+        let userId = this.props.router.params.userId ;
+        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/${userId}`)
+            .then(response => {
+            this.props.setUserProfile(response.data);
+        });
+    }
+    render() {
+        return (
+            <Profile {...this.props} profile={this.props.profile }/>
+        )
+    }
+}
+let mapStateToProps = (state)=>({
+    profile: state.profilePage.profile
+
+});
+function withRouter(Component) {
+    function ComponentWithRouterProp(props) {
+        let location = useLocation();
+        let navigate = useNavigate();
+        let params = useParams();
+        return (
+            <Component
+                {...props}
+                router={{ location, navigate, params }}
+            />
+        );
+    }
+
+    return ComponentWithRouterProp;
+}
+
+
+
+export default connect(mapStateToProps, {setUserProfile}) (withRouter(ProfileContainer));
